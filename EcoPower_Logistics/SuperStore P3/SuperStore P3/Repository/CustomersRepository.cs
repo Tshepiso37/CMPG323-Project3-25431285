08daa6276d06fd14fc1b1a500f6fa38671a57f8f
Tshepiso37 <91364465+Tshepiso37@users.noreply.github.com>
2023-09-14 11:49:34 +0200

    Code with Repositories
    In this code, the Repository folder has been created, with the GenericRepository and all the necessary Repository files needed.

---
+++ EcoPower_Logistics/SuperStore P3/SuperStore P3/Repository/CustomersRepository.cs
@@ -0,0 +1,23 @@
+﻿using Data;
+using Models;
+
+namespace EcoPower_Logistics.Repository
+{
+    public class CustomersRepository
+    {
+        protected readonly SuperStoreContext _context = new SuperStoreContext();
+
+        // GET ALL: Products
+        public IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // TO DO: Add ‘Get By Id’
+        // TO DO: Add ‘Create’
+        // TO DO: Add ‘Edit’
+        // TO DO: Add ‘Delete’
+        // TO DO: Add ‘Exists’
+
+    }
+}
---
+++ EcoPower_Logistics/SuperStore P3/SuperStore P3/Repository/iOrdersRepository.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace EcoPower_Logistics.Repository
+{
+    public interface iOrderRepository : iGenericRepository<Order>
+    {
+        Order GetMostRecentOrderDetail();
+    }
+}
